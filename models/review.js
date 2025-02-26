981a30b5b70bda3d6d036c1943528b84a5dbce8b
Joash <124445418+zhoda-lii@users.noreply.github.com>
2025-02-25 18:07:37 -0700

    Create review.js
    1.4 Store reviews & ratings
    
    Co-Authored-By: Lance <157422756+lancemiranoo@users.noreply.github.com>

---
+++ models/review.js
@@ -0,0 +1,11 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    return sequelize.define('Review', {
+        review_id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+        user_id: { type: DataTypes.INTEGER, allowNull: false },
+        property_id: { type: DataTypes.INTEGER, allowNull: false },
+        rating: { type: DataTypes.DECIMAL(2,1), allowNull: false, validate: { min: 1, max: 5 } },
+        review_text: { type: DataTypes.TEXT }
+    }, { timestamps: true });
+};
