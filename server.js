2a0991986130ccfaf52899567c765803d116742a
Joash <124445418+zhoda-lii@users.noreply.github.com>
2025-02-25 19:44:09 -0700

    Implemented server start logic

--- server.js
+++ server.js
@@ -247,3 +247,7 @@ app.delete('/reviews/:id', async (req, res) => {
     }
 });
 
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
