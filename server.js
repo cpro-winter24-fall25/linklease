4e9ca41c0f843ea8b4840d7dfbbda9805fde15d4
Joash <124445418+zhoda-lii@users.noreply.github.com>
2025-02-25 18:17:24 -0700

    Add user put API
    Co-Authored-By: Lance <157422756+lancemiranoo@users.noreply.github.com>

--- server.js
+++ server.js
@@ -39,3 +39,17 @@ app.get('/users/:id', async (req, res) => {
     }
 });
 
+app.put('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByPk(req.params.id);
+        if (user) {
+            await user.update(req.body);
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({ error: 'User not found' });
+        }
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+});
+
