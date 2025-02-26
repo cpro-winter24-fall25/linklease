86a1cadac62d5f5495c1fa69bd80797298e8b49d
Joash <124445418+zhoda-lii@users.noreply.github.com>
2025-02-25 18:16:38 -0700

    Add user get API
    Co-Authored-By: Lance <157422756+lancemiranoo@users.noreply.github.com>

--- server.js
+++ server.js
@@ -17,3 +17,12 @@ app.post('/users', async (req, res) => {
     }
 });
 
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.findAll();
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+});
+
